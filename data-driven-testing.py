65da863f3e0975430ca5262831427a06862c8026
Sandeep Tuniki <7530864+SandeepTuniki@users.noreply.github.com>
2020-09-05 16:10:17 +0530

    Create data-driven-testing.py

---
+++ data-driven-testing.py
@@ -0,0 +1,28 @@
+import pytest
+
+def is_positive(number):
+  if number >= 0:
+    return True
+  else:
+    return False
+
+
+def test_positive_case():
+  assert is_positive(23) == True
+
+
+def test_positive_case_1():
+  assert is_positive(23) == True
+
+def test_positive_case_2():
+  assert is_positive(10) == True
+
+
+@pytest.parameterize('number', [23, 10, 45, 0, 11])
+def test_positive_cases(number):
+  assert is_positive(number) == True
+
+
+@pytest.parameterize('number', [-5, -22, -13, -54])
+def test_positive_cases(number):
+  assert is_positive(number) == False
